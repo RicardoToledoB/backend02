1fe940e3efa60cadd188e4608e83a7fb9de7089a
ricardotoledob <ricardo.toledob@gmail.com>
2025-11-14 12:06:57 -0300

    jhashjdashjd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,61 +1,8 @@
 -- Región de Arica y Parinacota (XV)
 INSERT INTO communes (name, created_at, updated_at, deleted_at) VALUES
-                                                                    ('Arica', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Camarones', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Putre', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('General Lagos', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);
-
--- Región de Tarapacá (I)
-INSERT INTO communes (name, created_at, updated_at, deleted_at) VALUES
-                                                                    ('Iquique', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Alto Hospicio', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Pozo Almonte', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Camiña', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Colchane', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Huara', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Pica', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);
-
--- Región de Antofagasta (II)
-INSERT INTO communes (name, created_at, updated_at, deleted_at) VALUES
-                                                                    ('Antofagasta', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Mejillones', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Sierra Gorda', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Taltal', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Calama', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Ollagüe', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('San Pedro de Atacama', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Tocopilla', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('María Elena', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);
-
--- Región de Atacama (III)
-INSERT INTO communes (name, created_at, updated_at, deleted_at) VALUES
-                                                                    ('Copiapó', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Caldera', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Tierra Amarilla', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Chañaral', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Diego de Almagro', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Vallenar', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Alto del Carmen', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Freirina', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Huasco', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);
-
--- Región de Coquimbo (IV)
-INSERT INTO communes (name, created_at, updated_at, deleted_at) VALUES
-                                                                    ('La Serena', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Coquimbo', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Andacollo', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('La Higuera', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Paiguano', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Vicuña', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Illapel', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Canela', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Los Vilos', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Salamanca', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Ovalle', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Combarbalá', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Monte Patria', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Punitaqui', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
-                                                                    ('Río Hurtado', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);
+                                                                    ('Punta Arenas', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
+                                                                    ('Puerto Natales', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL),
+                                                                    ('Porvenir', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);
 
 
 INSERT INTO programs(name,created_at,updated_at,deleted_at) VALUES('PAB THOMAS FENTON',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,null);
